1b89bc43778bedded7ecafb2fafc3d41ef16c00a
jbeemster <joshua.a.beemster@gmail.com>
2023-07-29 12:54:56 +1200

    Prepared for release

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 locals {
   module_name    = "postgres-loader-pubsub-ce"
-  module_version = "0.2.1"
+  module_version = "0.3.0"
 
   app_name    = "snowplow-postgres-loader"
   app_version = var.app_version
